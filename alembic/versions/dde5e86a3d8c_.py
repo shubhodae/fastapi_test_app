2834e274c9cf2a46588c547c630ecdf3a818e041
shubhadip-indi <shubhadip.de@indiqube.com>
2023-07-05 12:22:01 +0530

    alembic migrations

---
+++ alembic/versions/dde5e86a3d8c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dde5e86a3d8c
+Revises: dd2aa6a10321
+Create Date: 2023-07-05 06:22:33.353653
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dde5e86a3d8c'
+down_revision = 'dd2aa6a10321'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
