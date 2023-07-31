9cbaea3b7d0d89d18fa69dc4154469ab17e1edce
shubhadip-indi <shubhadip.de@indiqube.com>
2023-07-31 12:31:34 +0530

    database url put in settings.py for both app and alembic

--- test_app/database.py
+++ test_app/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:tiger@db/test_db"
+from .settings import SQLALCHEMY_DATABASE_URL
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:tiger@db/test_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
