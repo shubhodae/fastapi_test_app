a6af2ef8ddfb2504fdb6bf0c7d0b09c976aa6ca4
shubhadip-indi <shubhadip.de@indiqube.com>
2023-08-29 08:06:16 +0000

    async implementation in items

--- items/helpers.py
+++ items/helpers.py
@@ -17,7 +17,7 @@ class ItemHandler(ModelHandler):
         self.user_id = user_id
 
 
-    def __fetch_item(self, item_id) -> Item:
+    async def __fetch_item(self, item_id) -> Item:
         item = self.db.query(Item).filter(
             Item.id == item_id,
             Item.owner_id == self.user_id,
@@ -31,7 +31,7 @@ class ItemHandler(ModelHandler):
         return item
 
 
-    def create(self, item_data: ItemSchema) -> Item:
+    async def create(self, item_data: ItemSchema) -> Item:
         item_obj = Item(**item_data.dict(), owner_id=self.user_id)
         self.db.add(item_obj)
         self.db.commit()
@@ -39,7 +39,7 @@ class ItemHandler(ModelHandler):
         return item_obj
 
 
-    def list(self) -> list[Item]:
+    async def list(self) -> list[Item]:
         item_query = self.db.query(Item).filter(
             Item.owner_id == self.user_id,
             Item.is_active == True
@@ -48,12 +48,12 @@ class ItemHandler(ModelHandler):
         return item_list
 
 
-    def get(self, item_id) -> Item:
-        return self.__fetch_item(item_id)
+    async def get(self, item_id) -> Item:
+        return await self.__fetch_item(item_id)
 
 
-    def update(self, item_id, item_data: ItemUpdateSchema) -> Item:
-        item_obj = self.__fetch_item(item_id)
+    async def update(self, item_id, item_data: ItemUpdateSchema) -> Item:
+        item_obj = await self.__fetch_item(item_id)
         item_dict = item_data.dict(exclude_unset=True)
         if 'id' in item_dict:
             del item_dict['id']
@@ -65,8 +65,8 @@ class ItemHandler(ModelHandler):
         return item_obj
 
 
-    def delete(self, item_id) -> ItemIDSchema:
-        item_obj = self.__fetch_item(item_id)
+    async def delete(self, item_id) -> ItemIDSchema:
+        item_obj = await self.__fetch_item(item_id)
         item_obj.is_active = False
         self.db.add(item_obj)
         self.db.commit()
--- items/views.py
+++ items/views.py
@@ -3,7 +3,6 @@ from typing import Annotated
 from sqlalchemy.orm import Session
 
 from test_app.database import get_db
-# from test_app.decorators import exception_handler_decorator
 from test_app.dependencies import get_current_user_id
 
 from .schemas import ItemSchema, ItemIDSchema, ItemUpdateSchema, ItemSchemaWithID
@@ -26,7 +25,7 @@ async def create_item(
     user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     handler_obj = ItemHandler(db=db, user_id=user_id)
-    item = handler_obj.create(item_data)
+    item = await handler_obj.create(item_data)
     return item
 
 
@@ -36,7 +35,7 @@ async def fetch_item_list(
     user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     handler_obj = ItemHandler(db=db, user_id=user_id)
-    return handler_obj.list()
+    return await handler_obj.list()
 
 
 @router.get("/{item_id}", response_model=ItemSchema)
@@ -46,7 +45,7 @@ async def fetch_item(
     user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     handler_obj = ItemHandler(db=db, user_id=user_id)
-    item = handler_obj.get(item_id)
+    item = await handler_obj.get(item_id)
     return item
 
 
@@ -58,7 +57,7 @@ async def update_item(
     user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     handler_obj = ItemHandler(db=db, user_id=user_id)
-    item = handler_obj.update(item_id, item_data)
+    item = await handler_obj.update(item_id, item_data)
     return item
 
 
@@ -69,5 +68,5 @@ async def delete_item(
     user_id: Annotated[int, Depends(get_current_user_id)]
 ):
     handler_obj = ItemHandler(db=db, user_id=user_id)
-    deleted_item = handler_obj.delete(item_id)
+    deleted_item = await handler_obj.delete(item_id)
     return deleted_item
